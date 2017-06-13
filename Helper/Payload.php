47a43c5d22da2df349fc19f38ce9e2baf7313bb0
Sagar Bhandari <sagar.bhandari@zipmoney.com.au>
2017-06-13 12:19:53 +1000

    Fixed the url builder issue in payload

--- Helper/Payload.php
+++ Helper/Payload.php
@@ -149,7 +149,7 @@ class Payload extends  AbstractHelper
     $this->_transactionCollection = $transactionCollection;
     $this->_logger = $logger;
     $this->_quoteFactory = $quoteFactory;
-    $this->_urlBuilder = $context->getUrl();
+    $this->_urlBuilder = $context->getUrlBuilder();
     $this->_helper = $helper;
   }
   
