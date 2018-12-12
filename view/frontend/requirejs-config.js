6f4d7a180c03f8068b178e3cfe1568f704ddb07f
ben.zhang <ben.zhang@zip.co>
2018-12-13 09:19:07 +1100

    Fixing the widget issue that heavy depend on jQuery and Fancy box, should be independent JS lib

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,10 +1,7 @@
 var config = {
     paths: {
-          zipMoneyWidgetJs: '//d3k1w8lx8mqizo.cloudfront.net/lib/js/zm-widget-js/dist/zipmoney-widgets-v1.min',
-          zipMoneyCheckoutJs:'https://static.zipmoney.com.au/checkout/checkout-v1.min'
-    },
-    shim: {
-      "zipMoneyWidgetJs": ["jquery"]
+        zipMoneyWidgetJs: 'https://static.zipmoney.com.au/lib/js/zm-widget-js/dist/zip-widget.min',
+        zipMoneyCheckoutJs:'https://static.zipmoney.com.au/checkout/checkout-v1.min'
     }
 };
 
