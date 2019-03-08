e0fa7c8880a4a654616651212fbf4aeb33ed6bd8
ben.zhang <ben.zhang@zip.co>
2019-03-08 12:56:43 +1100

    adding manual installation support without composer

--- registration.php
+++ registration.php
@@ -1,6 +1,9 @@
 <?php
+if (!class_exists("\zipMoney\Api\ChargesApi") && file_exists(BP . "/lib/internal/merchantapi-php/autoload.php")) {
+    require_once(BP . "/lib/internal/merchantapi-php/autoload.php");
+}
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'ZipMoney_ZipMoneyPayment',
     __DIR__
-); 
+);
