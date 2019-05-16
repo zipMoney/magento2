e0afc7b64a94f4a85975512d938e361a33195dfa
ben.zhang <ben.zhang@zip.co>
2019-05-16 17:47:15 +1000

    fixing terms and condition checkbox not working issue

--- view/frontend/web/js/view/payment/method-renderer/zipmoney-zipmoneypayment.js
+++ view/frontend/web/js/view/payment/method-renderer/zipmoney-zipmoneypayment.js
@@ -11,43 +11,43 @@
 /*browser:true*/
 /*global define*/
 
-var isLive = (window.checkoutConfig.payment.zipmoneypayment.environment == "production" );
+var isLive = (window.checkoutConfig.payment.zipmoneypayment.environment == "production");
 var inContextCheckoutEnabled = window.checkoutConfig.payment.zipmoneypayment.inContextCheckoutEnabled;
 
 define(
-    [   'Magento_Checkout/js/view/payment/default',
-        'ZipMoney_ZipMoneyPayment/js/action/place-zip-order',        
+    ['Magento_Checkout/js/view/payment/default',
+        'ZipMoney_ZipMoneyPayment/js/action/place-zip-order',
         'ZipMoney_ZipMoneyPayment/js/action/set-payment-method',
         'Magento_Ui/js/model/messages',
-        'ko',        
+        'ko',
         'Magento_Checkout/js/model/quote',
-        'jquery',        
-        'Magento_Checkout/js/model/error-processor',        
-        'Magento_Checkout/js/model/full-screen-loader',          
+        'jquery',
+        'Magento_Checkout/js/model/error-processor',
+        'Magento_Checkout/js/model/full-screen-loader',
         'Magento_Checkout/js/model/payment/additional-validators',
         'mage/storage',
         'zipMoneyCheckoutJs'
     ],
-    function (Component, placeZipOrderAction, setPaymentMethodAction, Messages,ko,quote,$,errorProcessor,fullScreenLoader,additionalValidators,storage) {
+    function (Component, placeZipOrderAction, setPaymentMethodAction, Messages, ko, quote, $, errorProcessor, fullScreenLoader, additionalValidators, storage) {
         'use strict';
 
-        return Component.extend({              
-            isPlaceOrderActionAllowed: ko.observable(quote.billingAddress() != null),             
-            redirectAfterPlaceOrder: true, 
+        return Component.extend({
+            isPlaceOrderActionAllowed: ko.observable(quote.billingAddress() != null),
+            redirectAfterPlaceOrder: true,
             defaults: {
                 template: 'ZipMoney_ZipMoneyPayment/payment/zipmoney'
             },
-            setupWidget: function () {                    
-                if(window.$zmJs!=undefined){
+            setupWidget: function () {
+                if (window.$zmJs != undefined) {
                     window.$zmJs._collectWidgetsEl(window.$zmJs);
                 }
             },
-            initChildren: function () {         
+            initChildren: function () {
                 this.messageContainer = new Messages();
                 this.createMessagesComponent();
                 return this;
-            }, 
-            continueToZipMoney: function (x,event) {
+            },
+            continueToZipMoney: function (x, event) {
                 var self = this,
                     placeOrder;
 
@@ -59,13 +59,13 @@ define(
 
                     this.isPlaceOrderActionAllowed(false);
                     this.selectPaymentMethod();
-                    
+
                     setPaymentMethodAction(this.messageContainer)
-                        .success(function(){
-                            placeZipOrderAction(self.getData(),self.messageContainer)
+                        .success(function () {
+                            placeZipOrderAction(self.getData(), self.messageContainer)
                         });
                 }
-                
+
                 return true;
             },
             placeOrder: function (data, event) {
@@ -80,26 +80,49 @@ define(
                     this.isPlaceOrderActionAllowed(false);
                     this.selectPaymentMethod();
 
-                    setPaymentMethodAction(this.messageContainer).success(function(){
-                        placeZipOrderAction(self.getData(),self.messageContainer)
+                    setPaymentMethodAction(this.messageContainer).success(function () {
+                        placeZipOrderAction(self.getData(), self.messageContainer)
                     });
                     return true;
                 }
                 return false;
             },
-            getPaymentAcceptanceMarkSrc: function() {
+            getAgreements: function (data) {
+                var agreementForm = $('.payment-method._active div[data-role=checkout-agreements] input');
+                if (typeof agreementForm !== 'undefined') {
+                    data['extension_attributes'] = {};
+                    var agreementData = agreementForm.serializeArray();
+                    var agreementIds = [];
+                    agreementData.forEach(function (item) {
+                        agreementIds.push(item.value);
+                    });
+                    data['extension_attributes']['agreement_ids'] = agreementIds;
+                }
+                return data;
+            },
+            getData: function () {
+                var data = {
+                    'method': this.getCode(),
+                };
+
+                data['additional_data'] = _.extend(data['additional_data'], this.additionalData);
+                data = this.getAgreements(data);
+
+                return data;
+            },
+            getPaymentAcceptanceMarkSrc: function () {
                 return window.checkoutConfig.payment.zipmoneypayment.paymentAcceptanceMarkSrc;
             },
-            getTitle:function(){
+            getTitle: function () {
                 return window.checkoutConfig.payment.zipmoneypayment.title;
-            }, 
-            getContinueText:function(){
+            },
+            getContinueText: function () {
                 return "Continue";
             },
-            getCode: function() {
+            getCode: function () {
                 return window.checkoutConfig.payment.zipmoneypayment.code;
             },
-            isActive: function() {
+            isActive: function () {
                 return true;
             }
         });
